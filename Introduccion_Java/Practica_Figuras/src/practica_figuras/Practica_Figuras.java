54972059283fc9c80b2a83f73f249462777607bb
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-09-12 23:19:59 -0600

    feat: crear interfaz

--- Introduccion_Java/Practica_Figuras/src/practica_figuras/Practica_Figuras.java
+++ Introduccion_Java/Practica_Figuras/src/practica_figuras/Practica_Figuras.java
@@ -1,12 +1,28 @@
 
 package practica_figuras;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
 
-public class Practica_Figuras {
+
+public class Practica_Figuras extends JFrame implements ActionListener {
+    
+    public Practica_Figuras(){
+        this.setBounds(0,0,400,400);
+        this.setLayout(null);
+        this.setVisible(true);
+    }
 
 
     public static void main(String[] args) {
+        Practica_Figuras ob = new Practica_Figuras();
+        ob.setVisible(true);
         
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
 }
