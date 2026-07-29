31416ce6c14aaf0701f166bb04bfade0434c4cd3
Ricardo <ricardo.macias1755@alumnos.udg.mx>
2026-07-29 01:49:48 -0600

    feat: agregar primera practica del planificador de multiples colas

---
+++ Planificador_Multiples_colas/Practica_1.py
@@ -0,0 +1,34 @@
+import time
+from CTkMessagebox import CTkMessagebox
+import customtkinter as CTk
+
+class progressBar(CTk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.geometry("300x300")
+        self.title("Barra de progreso")
+        
+        self.bar = CTk.CTkProgressBar(self, orientation="vertical", width=20, height=200)
+        self.bar.place(x=140, y=10)
+        self.bar.set(0)
+
+        CTk.CTkButton(self, text="Iniciar", command=self.start, width=40, height=30).place(x=125, y=220)
+    
+    def start(self):
+        iter_step = 1 / 100
+        progress_step = iter_step
+        self.bar.start()
+
+        for count in range(100):
+            self.bar.set(progress_step)
+            progress_step += iter_step
+            self.update_idletasks()
+        
+        self.bar.stop()
+
+        
+if __name__ == "__main__":
+
+    app = progressBar()
+    app.mainloop()
