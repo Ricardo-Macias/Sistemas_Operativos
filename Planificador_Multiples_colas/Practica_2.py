74356c039ec2b1df51e329a0a253ce3db2887bb6
Ricardo-Macias <ricardo.macias1755@alumnos.udg.mx>
2026-07-29 15:50:54 -0600

    feat: terminar practica 2 de planificador de multiples colas

--- Planificador_Multiples_colas/Practica_2.py
+++ Planificador_Multiples_colas/Practica_2.py
@@ -3,36 +3,42 @@ import time
 import datetime
 import customtkinter
 
-def process(name, size, count):
-    app = customtkinter.CTk()
-    app.title(name)
-    app.geometry(size)
+def process(label,count):
 
-    point = "."
-
-    for count_i in range(count):
-        lbl_point = customtkinter.CTkLabel(app, text=point)
-        lbl_point.pack()
-        point += "."
+    for i in range(count):
         time.sleep(1)
-        app.update()
-        lbl_point.destroy()
-    
-    app.mainloop()
+
+        current_text = label.cget("text")
+        new_text = current_text + "."
+
+        label.configure(text=new_text)
+        
 
 
 if __name__ == "__main__":
+    app = customtkinter.CTk()
+    app.title("Hilos")
+    app.geometry("200x200")
     start = datetime.datetime.now()
 
-    thread_1 = threading.Thread(name="hilo_1", target=process, args=("Proceso 1", "200x200", 7, ))
-    thread_2 = threading.Thread(name="hilo_2", target=process, args=("Proceso 2", "200x200", 5, ))
+    frame_thread_1 = customtkinter.CTkFrame(app)
+    frame_thread_1.pack(side="left", fill="both", expand=True)
+    frame_thread_2 = customtkinter.CTkFrame(app)
+    frame_thread_2.pack(side="right", fill="both", expand=True)
+
+    lbl_thread_1 = customtkinter.CTkLabel(frame_thread_1, text="Hilo 1: ")
+    lbl_thread_1.pack()
+    lbl_thread_2 = customtkinter.CTkLabel(frame_thread_2, text="Hilo 2: ")
+    lbl_thread_2.pack()
+
+    thread_1 = threading.Thread(name="hilo_1", target=process, args=(lbl_thread_1, 7, ))
+    thread_2 = threading.Thread(name="hilo_2", target=process, args=(lbl_thread_2, 5, ))
 
     thread_1.start()
     thread_2.start()
 
-    thread_1.join()
-    thread_2.join()
+    app.mainloop()
 
     end = datetime.datetime.now()
-
     print(str( end.second - start.second ))
+    app.mainloop()
